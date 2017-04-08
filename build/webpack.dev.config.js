845c23343010e04a8354765b14f5a2ed7066e6e5
lastIndexOf <kgaikj2cu@icloud.com>
2017-04-08 18:46:10 +0800

    修改配置及设置目录

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack')
+		, htmlWebpackPlugin = require('html-webpack-plugin')
+		, path = require('path')
+
+const hotMiddlewareScript = 'webpack-hot-middleware/client?reload=true'
+
+module.exports = {
+	// context: __dirname,
+	entry: {
+		main: ['./src/main.js', hotMiddlewareScript]
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: './js/[name].js',
+		publicPath: '/'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						query: {
+							presets: ['latest']
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'index.html',
+			inject: true
+		}),
+		new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+	]
+}
